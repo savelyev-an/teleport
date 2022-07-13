eb58aa0c143ec4d0c65e30b6a8bb0c1c95500293
EdwardDowling <edward.dowling@goteleport.com>
2022-07-13 11:30:20 +0100

    Refactor session listing code and remove tool_utils.go

--- tool/tsh/tsh_test.go
+++ tool/tsh/tsh_test.go
@@ -54,6 +54,7 @@ import (
 	"github.com/gravitational/teleport/lib/tlsca"
 	"github.com/gravitational/teleport/lib/utils"
 	"github.com/gravitational/teleport/lib/utils/prompt"
+	"github.com/gravitational/teleport/tool/common"
 
 	"github.com/ghodss/yaml"
 	"github.com/gravitational/trace"
@@ -2777,7 +2778,7 @@ func TestShowSessions(t *testing.T) {
 		},
 	}
 	var buf bytes.Buffer
-	err := client.ShowSessions(sessions, teleport.JSON, &buf)
+	err := common.ShowSessions(sessions, teleport.JSON, &buf)
 	require.NoError(t, err)
 	require.Equal(t, expected, buf.String())
 }
