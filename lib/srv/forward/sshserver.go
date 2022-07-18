cd76b5637c3f49c5b6975c584ef7e7f32656eb64
Joel Wejdenstål <jwejdenstal@icloud.com>
2022-07-18 13:38:05 +0200

    remove default

--- lib/srv/forward/sshserver.go
+++ lib/srv/forward/sshserver.go
@@ -346,7 +346,6 @@ func (s *Server) TargetMetadata() apievents.ServerMetadata {
 		ServerNamespace: s.GetNamespace(),
 		ServerID:        s.targetID,
 		ServerAddr:      s.targetAddr,
-		ServerLabels:    nil,
 		ServerHostname:  s.targetHostname,
 		Forwarded:       true,
 	}
